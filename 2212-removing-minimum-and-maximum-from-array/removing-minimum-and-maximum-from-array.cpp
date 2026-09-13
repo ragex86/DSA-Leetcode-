4b1b5136058b739d20aac84e84badb802faad4d5
ragex86 <kunalrawat591@gmail.com>
2026-09-13 13:01:18 +0530

    Time: 8 ms (6.29%) | Memory: 92.1 MB (62.17%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+     int n = nums.size();
+     int minelin = min_element(nums.begin(),nums.end())-nums.begin();
+     int maxelin = max_element(nums.begin(),nums.end())-nums.begin();
+
+     int left = min(minelin,maxelin);
+     int right = max(minelin,maxelin);
+
+     return min({ left+1+n-right,n-left,right+1 });
+
+    }
+    
+
+};

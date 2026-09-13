75f9fba2020ff4a383058555b8382a98b310ca32
villain xd <kunalrawat591@gmail.com>
2026-09-13 22:46:44 +0530

    Time: 43 ms (82.88%) | Memory: 29.1 MB (73.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -18,8 +18,7 @@ public:
                 }else if(sum>0){
                     k--;
                 }else{
-                    vector<int>temp = {nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
+                    ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
 
